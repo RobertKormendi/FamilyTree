2e316215da27bd5f231a206f33e11e271a003530
RobertKormendi <k.robi@outlook.com>
2025-06-07 00:18:47 -0400

    *completed familyTree.py
    *as in ready to submit
    Completed the print message algorithm, removed old commented code, added (hopefully) useful comments, made the code recursive (so you don't have to restart it every prompt) and moved function calls to a readable location. Tried cutting back on global variables but it didn't work first try so I left it.

--- familyTree.py
+++ familyTree.py
@@ -5,45 +5,36 @@ class Person():
         self.children = []
         self.parent = parent
     
-    def add_child(self, child):
-        self.children.append(child)
 
 
 
 family = [
-Person('Elizabeth', None),
-Person('Marj','Elizabeth'),
-Person('Rozanne','Elizabeth'),
-Person('Ruthanne','Elizabeth'),
-Person('Donnie','Elizabeth'),
-Person('Patricia','Elizabeth'),
-Person('Kim','Marj'),
-Person('Monica','Marj'),
-Person('Martin','Marj'),
-Person('Gabe','Marj'),
-Person('John','Marj'),
-Person('Ben','Marj'),
-Person('Jerome','Marj'),
-Person('John G', 'Kim'),
-Person('Jamie', 'Kim'),
-Person('Julia', 'Kim'),
-Person('Phil', 'Monica'),
-Person('Alley', 'Monica'),
-Person('Anna', 'Monica'),
-Person('Steven', 'Monica'),
-Person('Lincoln', 'Phil'),
-Person('Ellane', 'Gabe'),
-Person('Mo', 'Ellane'),
-Person('Jake', 'Gabe'),
-Person('Jeera', 'Jerome'),
-Person('Precila', 'Jerome'),
-Person('Clarence', 'Jerome')
+Person("Adam", None),
+Person("Beth", "Adam"),
+Person("Carl", "Adam"),
+Person("Dana", "Carl"),
+Person("Eve", "Carl"),
+Person("Frank", "Dana"),
+Person("Grace", "Eve"),
+Person("Hank", "Dana"),
+Person("Ivy", "Eve"),
+Person("Jack", "Grace"),
+Person("Kara", "Grace"),
+Person("Liam", "Ivy"),
+Person("Mona", "Liam"),
+Person("Nate", "Jack"),
+Person("Olive", "Kara"),
+Person("Pete", "Olive"),
+Person("Quinn", "Mona"),
+Person("Rita", "Liam"),
+Person("Sam", "Quinn"),
+Person("Tina", "Jack")
 ]
 
 
 
-
-for person in family:
+#assign each person their parent's object rather than just a string
+for person in family: 
     for parent in family:
         if person.parent is None:
             break;
@@ -53,14 +44,9 @@ for person in family:
 
 
 
-# for each in family:
-#     for person in family:
-#         if each.name == person.parent:
-#             each.add_child(person.name)
-#             person.gen = each.gen + 1
 
 
-def findGeneration(x):
+def findGeneration(x): #assign each member their generation number
     gen = 1
     person = family[x]
     while person.parent != None:
@@ -72,95 +58,117 @@ def findGeneration(x):
 for x in range(len(family)):
     family[x].gen = findGeneration(x)
 
-# find index of each input
-input = input("Enter a name: ").split()
-for x in range(len(input)):
-    for i in range(len(family)):
-        if family[i].name == input[x]:
-            input[x] = i
-            break
-        elif i == len(family) - 1:
-            print(f"Name {input[x]} not found")
-            input.pop(x)
+
+def getInput():
+
+    while True:
+        error  = False
+        myInput = input("Enter two comma separated names: ").replace(" ", "").split(',') # map input to a list
+        if len(myInput) == 2: #make sure that 2 names were given
+            for x in range(len(myInput)):
+                for i in range(len(family)):
+                    if family[i].name == myInput[x]:
+                        myInput[x] = family[i]
+                        break
+                    elif i == len(family) - 1:
+                        print(f"Error! Name {myInput[x]} not found")
+                        error = True 
+                        break;
+        else:
+            print("make sure they're comma separated")
+            error = True
+        if error == False: #only exit the loop if a valid input is given
+            break;
+    return(myInput[0], myInput[1])
 
 
-# for x in range(len(input)):
-#     print(f"{family[input[x]].name}")
-#     print(f"{family[input[x]].gen}")
 
 
-def higherOrLower(i1, i2):
-    p1 = family[i1]
-    p2 = family[i2]
+def higherOrLower(p1, p2): #find which person has the higher generation number
     
     if p1.gen > p2.gen:
         higher = p1
         lower = p2
+        
     else:
         higher = p2
-        lower = p1        
+        lower = p1
         
-    return([higher, lower])
-    
-    # for x in range(higher.gen - lower.gen):
-    #     check = check.parent
-        
-    # return(check.name)
+    return(higher, lower)
 
 
-myList = higherOrLower(input[0], input[1])
 
-def findAncestor(higher, lower):
+def findAncestor(higher, lower): 
+    if higher == lower: # check if they are the same person
+        return(0) # 0 is an arbitrary value that I use to check in the end
     check = higher
     
     for x in range(higher.gen - lower.gen):
-        check = check.parent #find the person in higher's lineage that is in lower's generation. If that is the same person as lower, then its solved
+        check = check.parent #find the person in higher's lineage that is in lower's generation.
         
-    if check == lower:
-        return(1)
+    if check == lower: #check if the person in higher's lineage at lower's generation is lower
+        return(1) #also arbitrary
         
     else:
-        check2 = lower
-        for x in range(lower.gen):
-            check2 = check2.parent
-            check = check.parent
+        check2 = lower 
+        while check.parent is not None: #make sure that the loop ends at ancestor
+            check2 = check2.parent #iterate over the lineages of both people
+            check = check.parent   #until a common ancestor is found
             if check == check2:
                 break; #check is now the common ancestor
                 
-    difference1 = higher.gen - check.gen
-    difference2 = lower.gen - check.gen
-    return([difference1, difference2])
+    difference1 =  person1.gen - check.gen 
+    difference2 = person2.gen - check.gen
+    return([difference1, difference2]) #returns the difference in generation between each person and the common ancestor
             
-genDifferences = findAncestor(myList[0], myList[1])
 
 def getSuffix(n):
     
-    if 10 <= n % 100 <= 20:
+    if 10 <= n % 100 <= 20: #special cases between 10 and 20
         suffix = 'th'
         
     else: suffix = {1: "st", 2: "nd", 3: "rd"}.get(n % 10, 'th')
-    return(suffix)
+    return(suffix) #otherwise check if they end with 1, 2 or 3
+    
+def numToTimes(n):
+    suffixes = {1: "once", 2: "twice", 3: "thrice"}
+    return suffixes.get(n, f"{n} times") 
+    
     
 def getRelation(differences):
-    if differences == 1:
-        genDiff = myList[0].gen - myList[1].gen
-        if genDiff == 1:
-            return 'child'
-        elif genDiff == 2:
-            return 'grandchild'
-        elif genDiff > 2:
-            return "great " * (genDiff - 2) + "grandchild"
-
-print(getRelation(genDifferences))
-    
-
-
-    
-    
-    
-
-# def checkRelationship(x, y):
-#     if family[x].name == family[y].name:
-#         print(f"{family[x].name} is the same person as {family[y].name}")
-    
+    if differences == 0: #arbitrary number is used here
+        return(f"{person1.name} and {person2.name} are the same person")
         
+# if they are of the same lineage, use the difference in their generation numbers to assign the correct message
+    elif differences == 1: 
+        genDiff = person1.gen - person2.gen
+        if genDiff == 1:
+            return(f" {person1.name} is the child of {person2.name}")
+        elif genDiff == 2:
+            return (f" {person1.name} is the grandchild of {person2.name}")
+        elif genDiff > 2:
+            return(f"{person1.name} is the {"great " * (genDiff - 2)}grandchild of {person2.name}")
+        elif genDiff == -1:
+            return(f" {person1.name} is the parent of {person2.name}")
+        elif genDiff == -2:
+            return(f" {person1.name} is the grandparent of {person2.name}")
+        elif genDiff < -2:
+            return(f"{person1.name} is the {"great " * (genDiff - 2)}grandparent of {person2.name}")            
+    else:
+        cousin = differences[0] #assign how manyth cousin
+        timesRemoved = differences[1] #assign how many times removed they are
+        
+        #call getSuffix() and numToTimes() to get the correct numbers in the print message
+        return(f"{person1.name} and {person2.name} are {cousin}{getSuffix(cousin)} cousins {numToTimes(timesRemoved)} removed ")
+   
+while True:
+    person1, person2 = getInput() #call input function
+    myList = higherOrLower(person1, person2)
+    genDifferences = findAncestor(myList[0], myList[1])
+    #call getRelation() to find the print message
+    print(getRelation(genDifferences))
+    goAgain = input("Type 'yes' (or anything) to check another pair, or 'no' to exit")
+    if goAgain == "no":
+        break;
+    
+    
